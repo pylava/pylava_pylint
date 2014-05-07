4ab56330d26440b964dc2ae33e0499f39273bf55
Kirill Klenov <horneds@gmail.com>
2014-05-07 12:42:28 +0700

    Update version

--- setup.py
+++ setup.py
@@ -8,29 +8,33 @@ pylama_pylint
 pylama_pylint -- Pylint integration to pylama library.
 
 """
-
+import re
 from os import path as op
 
 from setuptools import setup, find_packages
 
-from sys import version_info
 
-
-def __read(fname):
+def _read(fname):
     try:
         return open(op.join(op.dirname(__file__), fname)).read()
     except IOError:
         return ''
 
+_meta = _read('pylama_pylint/__init__.py')
+_license = re.search(r'^__license__\s*=\s*"(.*)"', _meta, re.M).group(1)
+_project = re.search(r'^__project__\s*=\s*"(.*)"', _meta, re.M).group(1)
+_version = re.search(r'^__version__\s*=\s*"(.*)"', _meta, re.M).group(1)
+
+from sys import version_info
 if version_info >= (3, 0):
     raise NotImplementedError("Pylint doesnt support python3.")
 
 setup(
-    name='pylama_pylint',
-    version='0.1.6',
-    license='BSD',
-    description=__read('DESCRIPTION'),
-    long_description=__read('README.rst'),
+    name=_project,
+    version=_version,
+    license=_license,
+    description=_read('DESCRIPTION'),
+    long_description=_read('README.rst'),
     platforms=('Any'),
 
     author='horneds',
@@ -51,9 +55,7 @@ setup(
     package_data={'pylama_pylint': ['pylint.rc',
                                     'astroid/brain/py2stdlib.py']},
     install_requires=[
-        l for l in __read('requirements.txt').split('\n')
+        l for l in _read('requirements.txt').split('\n')
         if l and not l.startswith('#')],
     test_suite='tests',
 )
-
-# lint_ignore=F0401
