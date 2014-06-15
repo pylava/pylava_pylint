f76ea61c68722294b44ff034292c3d32667d913c
Kirill Klenov <horneds@gmail.com>
2014-06-15 19:19:57 +0400

    Fix tests

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ def test_pylint():
     from pylama.config import parse_options
 
     options = parse_options(linters=['pylint'], config=False)
-    options.ignore = set(['R0912'])
+    options.ignore = set(['R0912', 'C0111'])
     errors = run('pylama_pylint/pylint/utils.py', options=options)
     assert len(errors) == 23
     assert errors[0].number == 'W0622'
