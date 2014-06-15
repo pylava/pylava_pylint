b3d2122c9e7edcd94e6a7935c229e8e9f0ef9010
Kirill Klenov <horneds@gmail.com>
2014-06-15 18:11:47 +0400

    Better handling pylint options from pylama config files

--- tests.py
+++ tests.py
@@ -9,9 +9,19 @@ def test_pylint():
     from pylama.config import parse_options
 
     options = parse_options(linters=['pylint'], config=False)
+    options.ignore = set(['R0912'])
     errors = run('pylama_pylint/pylint/utils.py', options=options)
-    assert len(errors) == 25
-    assert errors[0].number == 'W00622'
+    assert len(errors) == 23
+    assert errors[0].number == 'W0622'
+
+    options.linters_params['pylint'] = dict(disable="W")
+    errors = run('pylama_pylint/pylint/utils.py', options=options)
+    assert len(errors) == 19
+    assert errors[0].number == 'C0330'
+
+    options.linters_params['pylint']['max_line_length'] = 200
+    errors = run('pylama_pylint/pylint/utils.py', options=options)
+    assert len(errors) == 3
 
 
 # pylama:ignore=D
