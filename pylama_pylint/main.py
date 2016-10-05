fa0eff470abb30e9ae8048077d175f5f07e66fe2
Oliver Berger <oliver@digitalarchitekt.de>
2016-10-05 12:01:15 +0200

    fixed Reporter class to be compatible with pylint==1.6.4

--- pylama_pylint/main.py
+++ pylama_pylint/main.py
@@ -39,22 +39,24 @@ class Linter(BaseLinter):
             def _display(self, layout):
                 pass
 
-            def add_message(self, msg_id, location, msg):
-                _, _, line, col = location[1:]
+            def handle_message(self, msg):
                 self.errors.append(dict(
-                    lnum=line,
-                    col=col,
-                    text="%s %s" % (msg_id, msg),
-                    type=msg_id[0]
+                    lnum=msg.line,
+                    col=msg.column,
+                    text="%s %s" % (msg.msg_id, msg.msg),
+                    type=msg.msg_id[0]
                 ))
 
         params = _Params(ignore=ignore, select=select, params=params)
         logger.debug(params)
 
-        runner = Run(
-            [path] + params.to_attrs(), reporter=Reporter(), exit=False)
+        reporter = Reporter()
 
-        return runner.linter.reporter.errors
+        runner = Run(
+            [path] + params.to_attrs(), reporter=reporter, exit=False
+        )
+
+        return reporter.errors
 
 
 class _Params(object):
